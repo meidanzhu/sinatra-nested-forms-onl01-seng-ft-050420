bd6bfef60825ae5513d19e593ffe7ea7ea246eb5
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-06-21 02:21:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,14 +4,22 @@ module FormsLab
   class App < Sinatra::Base
 
     get '/' do
+      erb :"root"
     end
 
     get '/new' do
       erb :"pirate/new"
     end
 
-    post '/pirates' do
-      erb :"pirates/show"
+    post "/pirates" do
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+      @ships = Ship.all
+
+      erb :'pirates/show'
     end
 
   end
